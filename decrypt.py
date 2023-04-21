c5d8093028ba6f30d2129a0343422b1e2f564926
keixjk <kayegallogo@gmail.com>
2023-04-22 04:51:09 +0800

    asked user for a string input

---
+++ decrypt.py
@@ -0,0 +1,12 @@
+# Pseudocode
+# prompt the user for input
+encrypted_text = input("Enter a string to decrypt: ")
+def decrypt_string(encrypted_text):
+    decrypted_text = "\033[1;32m"
+# check every character
+# if *, change to a
+# if &, change to e 
+# if #, change to i
+# if +, change to o
+# if !, change to u
+# print output
