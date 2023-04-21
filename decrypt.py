3e09158b5314c3eae408473ceeae48453d1eb49e
keixjk <kayegallogo@gmail.com>
2023-04-22 05:02:32 +0800

    if character is +, change to e

--- decrypt.py
+++ decrypt.py
@@ -4,11 +4,13 @@ encrypted_text = input("Enter a string to decrypt: ")
 def decrypt_string(encrypted_text):
     decrypted_text = "\033[1;32m"
 # check every character
-for char in encrypted_text:
+    for char in encrypted_text:
 # if *, change to a
-  if char == "*":
+        if char == "*":
             decrypted_text += "a"
 # if &, change to e 
+        elif char == "&":
+            decrypted_text += "e"
 # if #, change to i
 # if +, change to o
 # if !, change to u
